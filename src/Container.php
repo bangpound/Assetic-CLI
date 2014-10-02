9b6833817396dcb841f709c9fa2d6afd1744c574
Benjamin Doherty <bjd@bangpound.org>
2014-10-02 07:08:42 -0500

    pass array of values into container constructor.

--- src/Container.php
+++ src/Container.php
@@ -4,11 +4,10 @@ namespace Bangpound\Assetic;
 
 use Bangpound\Pimple\Provider\AsseticServiceProvider;
 use Pimple\Container as BaseContainer;
-use Symfony\Component\Config\Loader\LoaderInterface;
 
 class Container extends BaseContainer
 {
-    public function __construct(LoaderInterface $loader)
+    public function __construct(array $values = array())
     {
         $parameters = $loader->load('assetic.yml');
         parent::__construct($parameters['parameters']);
@@ -16,5 +15,7 @@ class Container extends BaseContainer
         foreach ($parameters['parameters']['providers'] as $class) {
             $this->register(new $class());
         }
+        // assetic.yml contains all container configuration.
+        parent::__construct($values);
     }
 }
