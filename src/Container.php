5db28efac2466ce6d88a7a99b9730e1879d90f51
Benjamin Doherty <bjd@bangpound.org>
2014-10-02 07:10:35 -0500

    application service provider.

--- src/Container.php
+++ src/Container.php
@@ -2,20 +2,16 @@
 
 namespace Bangpound\Assetic;
 
-use Bangpound\Pimple\Provider\AsseticServiceProvider;
+use Bangpound\Assetic\Provider\ApplicationServiceProvider;
 use Pimple\Container as BaseContainer;
 
 class Container extends BaseContainer
 {
     public function __construct(array $values = array())
     {
-        $parameters = $loader->load('assetic.yml');
-        parent::__construct($parameters['parameters']);
-        $this->register(new AsseticServiceProvider());
-        foreach ($parameters['parameters']['providers'] as $class) {
-            $this->register(new $class());
-        }
         // assetic.yml contains all container configuration.
         parent::__construct($values);
+
+        $this->register(new ApplicationServiceProvider());
     }
 }
